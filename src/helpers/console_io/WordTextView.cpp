75c0f16488dcf9361cc5bca3ed3302c71b49275c
Freaxed <andrea.anzani@gmail.com>
2023-01-30 14:52:02 +0100

    console io now can be clicked to open a file

--- src/helpers/console_io/WordTextView.cpp
+++ src/helpers/console_io/WordTextView.cpp
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <string>
 #include <Array.h>
+#include <Path.h>
+#include "storage/FindDirectory.h"
+#include <Window.h>
 
 bool Contains(std::string const &s, char ch) noexcept {
 	return s.find(ch) != std::string::npos;
@@ -17,6 +20,7 @@ constexpr bool IsASpace(int ch) noexcept {
 	return (ch == ' ') || ((ch >= 0x09) && (ch <= 0x0d));
 }
 
+std::string numericChars("1234567890");
 std::string kDefaultAdditionalWordCharacters(":@-+./_~");
 std::string wordCharacters("1234567890_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
@@ -61,6 +65,25 @@ WordTextView::_Reset()
 	
 	_SetStyle(false);
 }
+void	
+WordTextView::MouseDown (BPoint where)
+{
+	BTextView::MouseDown(where);
+	int32 pointPosition = OffsetAt(where);
+	if (pointPosition >= fStartPosition && pointPosition <= fStopPosition)
+	{
+		entry_ref  	ref;
+		if (get_ref_for_path(fCurrentPath.String(), &ref) == B_OK)
+		{
+			BMessage msg(B_REFS_RECEIVED);
+			msg.AddRef("refs", &ref);
+			if (fCurrentPositions[0] != 0)
+				msg.AddInt32("be:line", fCurrentPositions[0]);
+			
+			Window()->PostMessage(&msg);
+		}
+	}
+}
 
 void	
 WordTextView::MouseMoved (BPoint where, uint32 code, const BMessage *dragMessage)
@@ -69,13 +92,14 @@ WordTextView::MouseMoved (BPoint where, uint32 code, const BMessage *dragMessage
 	
 	
 	int32 pointPosition = OffsetAt(where);
-	//printf("%d %d %d\n", fStartPosition, pointPosition, fStopPosition);
 	if (pointPosition >= fStartPosition && pointPosition <= fStopPosition)
 		return;
 
 	int32 startPosition = -1;
 	int32 stopPosition = -1;
 	BString word = "";
+	fCurrentPath = "";
+	fCurrentPositions[0] = fCurrentPositions[1] = 0;
 	
 	_Reset();
 	bool found = _GetHyperLinkAt(where, pointPosition, word, startPosition, stopPosition);
@@ -92,7 +116,7 @@ WordTextView::MouseMoved (BPoint where, uint32 code, const BMessage *dragMessage
 void	
 WordTextView::OnWord(BString& newWord)
 {
-	//printf("New word: %s \n", newWord.String());
+	//printf("New path: %s line[%d] column[%d]\n", fCurrentPath.String(), fCurrentPositions[0], fCurrentPositions[1]);
 	_SetStyle(true);
 	
 }
@@ -147,117 +171,66 @@ WordTextView::_GetHyperLinkAt(BPoint where, const int32 pointPosition, BString& 
 	
 	GetText	(startPosition, len, fBuffer);
 	word.SetTo(fBuffer, len);
-	/*
-	//following code inspired by Terminal:
-
-	// Collect a list of colons in the string and their respective positions in
-	// the text buffer. We do this up-front so we can unlock the text buffer
-	// while we're doing all the entry existence tests.
 	
-	typedef Array<int32> ColonList;
-	ColonList colonPositions;
-	int32 searchPos = startPosition;
-
-
-	for (int32 index = 0; (index = word.FindFirst(':', index)) >= 0;) {
-		int32 foundStart;
-		int32 foundEnd;
-		if (!word.Find(":", searchPos, true, true, false, foundStart,
-				foundEnd)) {
-			return false;
-		}
-
-		CharPosition colonPosition;
-		colonPosition.index = index;
-		colonPosition.position = foundStart;
-		if (!colonPositions.Add(colonPosition))
-			return false;
-
+	// let's try to find the ':' chars and decide if we can extract line and column
+	// information.
+	int32 index = 0;
+	int32 firstIndex = -1;
+	int32 indexPosition = -1;
+	fCurrentPositions[0] = 0;
+	fCurrentPositions[1] = 0;
+	if ((index = word.FindFirst(':', index)) >= 0)
+	{
+		firstIndex = index;
+		indexPosition = 0;
 		index++;
-		searchPos = foundEnd;
-	}*/
-/*
-	textBufferLocker.Unlock();
-
-	// Since we also want to consider ':' a potential path delimiter, in two
-	// nested loops we chop off components from the beginning respective the
-	// end.
-	BString originalText = text;
-	TermPos originalStart = _start;
-	TermPos originalEnd = _end;
-
-	int32 colonCount = colonPositions.Count();
-	for (int32 startColonIndex = -1; startColonIndex < colonCount;
-			startColonIndex++) {
-		int32 startIndex;
-		if (startColonIndex < 0) {
-			startIndex = 0;
-			_start = originalStart;
-		} else {
-			startIndex = colonPositions[startColonIndex].index + 1;
-			_start = colonPositions[startColonIndex].position;
-			if (_start >= pos)
+		while(index < len) {
+			if(Contains(numericChars, fBuffer[index]))
+			{
+				fCurrentPositions[indexPosition] = fCurrentPositions[indexPosition] * 10 + (fBuffer[index] - '0');
+			}
+			else if (fBuffer[index] == ':')
+			{
+				indexPosition++;
+				if (indexPosition > 1)
+					break;
+			}
+			else
+			{
 				break;
-			_start.x++;
-				// Note: This is potentially a non-normalized position (i.e.
-				// the end of a soft-wrapped line). While not that nice, it
-				// works anyway.
-		}
-
-		for (int32 endColonIndex = colonCount; endColonIndex > startColonIndex;
-				endColonIndex--) {
-			int32 endIndex;
-			if (endColonIndex == colonCount) {
-				endIndex = originalText.Length();
-				_end = originalEnd;
-			} else {
-				endIndex = colonPositions[endColonIndex].index;
-				_end = colonPositions[endColonIndex].position;
-				if (_end <= pos)
-					break;
 			}
-
-			originalText.CopyInto(text, startIndex, endIndex - startIndex);
-			if (text.IsEmpty())
-				continue;
-
-			// check, whether the file exists
-			BString actualPath;
-			if (_EntryExists(text, actualPath)) {
-				_link = HyperLink(text, actualPath, HyperLink::TYPE_PATH);
-				return true;
-			}
-
-			// As such this isn't an existing path. We also want to recognize:
-			// * "<path>:<line>"
-			// * "<path>:<line>:<column>"
-
-			BString path = text;
-
-			for (int32 i = 0; i < 2; i++) {
-				int32 colonIndex = path.FindLast(':');
-				if (colonIndex <= 0 || colonIndex == path.Length() - 1)
-					break;
-
-				char* numberEnd;
-				strtol(path.String() + colonIndex + 1, &numberEnd, 0);
-				if (*numberEnd != '\0')
-					break;
-
-				path.Truncate(colonIndex);
-				if (_EntryExists(path, actualPath)) {
-					BString address = path == actualPath
-						? text
-						: BString(actualPath) << (text.String() + colonIndex);
-					_link = HyperLink(text, address,
-						i == 0
-							? HyperLink::TYPE_PATH_WITH_LINE
-							: HyperLink::TYPE_PATH_WITH_LINE_AND_COLUMN);
-					return true;
-				}
-			}
+			index++;
 		}
 	}
-*/
-	return true;
+	
+	if (firstIndex > 0)
+		word.Truncate(firstIndex);
+		
+	return _EntryExists(word, fCurrentPath);
 }
+
+bool
+WordTextView::_EntryExists(const BString& path,	BString& _actualPath) const
+{
+	if (path.IsEmpty())
+		return false;
+
+	if (path[0] == '/' || fCurrentDirectory.IsEmpty()) {
+		_actualPath = path;
+	} else if (path == "~" || path.StartsWith("~/")) {
+		// Replace '~' with the user's home directory. We don't handle "~user"
+		// here yet.
+		BPath homeDirectory;
+		if (find_directory(B_USER_DIRECTORY, &homeDirectory) != B_OK)
+			return false;
+		_actualPath = homeDirectory.Path();
+		_actualPath << path.String() + 1;
+	} else {
+		_actualPath.Truncate(0);
+		_actualPath << fCurrentDirectory << '/' << path;
+	}
+
+	struct stat st;
+	return lstat(_actualPath, &st) == 0;
+}
+

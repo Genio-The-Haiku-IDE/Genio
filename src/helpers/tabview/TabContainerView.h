32014b66c66038d6e6cd547ea00594997b99eb75
Jackburton79 <stefano.ceccherini@gmail.com>
2023-04-05 08:45:10 +0200

    Cleanup

--- src/helpers/tabview/TabContainerView.h
+++ src/helpers/tabview/TabContainerView.h
@@ -16,6 +16,12 @@ class TabView;
 
 #include "Draggable.h"
 
+enum {
+	MSG_CLOSE_TAB			= 'cltb',
+	MSG_CLOSE_TABS_ALL		= 'clta',
+	MSG_CLOSE_TABS_OTHER	= 'clto'
+};
+
 class TabContainerView : public BGroupView, public Draggable {
 public:
 	class Controller {

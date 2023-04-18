d5ec984e2d4d5db6effdc25d104bb8e7b533e9f7
Jackburton79 <stefano.ceccherini@gmail.com>
2023-04-18 09:01:42 +0200

    Style: add empty line between functions

--- src/GenioNamespace.cpp
+++ src/GenioNamespace.cpp
@@ -43,6 +43,7 @@ CompareVersion(const BString appVersion, const BString fileVersion)
 	// Should not get here
 	return 0;
 }
+
 BString
 GetSignature()
 {
@@ -50,7 +51,6 @@ GetSignature()
 	if (appSignature.IsEmpty()) {
 		char signature[B_MIME_TYPE_LENGTH];
 		app_info appInfo;
-
 		if (be_app->GetAppInfo(&appInfo) == B_OK) {
 			BFile file(&appInfo.ref, B_READ_ONLY);
 			if (file.InitCheck() == B_OK) {

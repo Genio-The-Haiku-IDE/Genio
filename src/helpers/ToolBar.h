ccfb42a7b35c19b989fc9edd77c9e7c6ba7bfcf0
Freaxed <andrea.anzani@gmail.com>
2023-04-21 15:53:40 +0200

    Replaced toolbar with Koder's ToolBar with resizable icons

---
+++ src/helpers/ToolBar.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Kacper Kasper <kacperkasper@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+
+#include <ToolBar.h>
+
+#include <string>
+#include <unordered_map>
+
+
+class BHandler;
+
+
+class ToolBar : public BPrivate::BToolBar {
+public:
+	ToolBar(BHandler* defaultTarget);
+
+	virtual void	Draw(BRect updateRect);
+
+	void			AddAction(uint32 command, const char* toolTipText,
+						const char* iconName = nullptr, bool lockable = false);
+	void			ChangeIconSize(float newSize);
+
+private:
+	BHandler*	fDefaultTarget;
+	float		fIconSize;
+	// command, iconName
+	std::unordered_map<uint32, std::string> fActionIcons;
+};
+
+
+#endif // TOOLBAR_H

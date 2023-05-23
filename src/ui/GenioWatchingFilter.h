0b4621d3a449f09d9e4e71bc4ca377b2c0efdd16
Freaxed <andrea.anzani@gmail.com>
2023-05-23 08:58:37 +0200

    added more comments

--- src/ui/GenioWatchingFilter.h
+++ src/ui/GenioWatchingFilter.h
@@ -9,6 +9,10 @@
 #include <PathMonitor.h>
 #include "Log.h"
 
+// This is attached to the PathMonitor class to avoid watching too many (useless) files.
+// In the ProjectFolderBrowser we care only on directories and related files events.
+// We can't use the B_WATCH_DIRECTORIES_ONLY flag as PathMonitor won't notify for file related events.
+
 class GenioWatchingFilter : public BPrivate::BPathMonitor::BWatchingInterface {
 public:
 
@@ -20,7 +24,7 @@ public:
 		BDirectory dir(node);
 		if ((status = dir.InitCheck()) != B_OK) {
 			// Typically the reason for this failure is "Not a directory".
-			// As we want to avoid to use a watch_node for standard file,
+			// As we want to avoid to use a watch_node for every standard file,
 			// we quit here.
 			return B_OK;
 		}

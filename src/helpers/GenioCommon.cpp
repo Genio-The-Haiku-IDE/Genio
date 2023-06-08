6dd4b4b0efe14604b11fb659c7fe4b03e90a4601
Freaxed <andrea.anzani@gmail.com>
2023-06-08 11:33:05 +0200

    removed unused/deprecated code

--- src/helpers/GenioCommon.cpp
+++ src/helpers/GenioCommon.cpp
@@ -16,58 +16,6 @@
 namespace Genio
 {
 
-std::string
-const Copyright()
-{
-	std::ostringstream header;
-	header
-		<< "/*\n"
-		<< " * Copyright " << get_year() << " Your_Name <your@email.address>\n"
-		<< " * All rights reserved. Distributed under the terms of the MIT license.\n"
-		<< " */\n";
-
-	return header.str();
-}
-
-/*
- * Returns the string to be used in header guard
- * In: ALLUPPER  = Do not modify
- *     all_lower = ALL_LOWER
- *     CamelCase = CAMEL_CASE
- */
-std::string const
-HeaderGuard(const std::string& fileName)
-{
-	std::string upper(fileName);
-	std::string lower(fileName);
-	std::transform (upper.begin(), upper.end(), upper.begin(), ::toupper);
-	std::transform (lower.begin(), lower.end(), lower.begin(), ::toupper);
-
-	// Filename is UPPER case, leave it
-	if (fileName == upper)
-		return fileName;
-	// Filename is lower case, make it UPPER
-	else if (fileName == lower)
-		return upper;
-
-	std::string in(fileName);
-	std::string out("");
-
-	for (auto iter = in.begin(); iter != in.end(); iter++) {
-		if (iter != in.begin() && std::isupper(static_cast<unsigned char>(*iter)))
-			out += static_cast<unsigned char>('_');
-		out += std::toupper(static_cast<unsigned char>(*iter));
-	}
-	return out;
-}
-
-bool
-file_exists(const std::string& filename)
-{
-	// TODO: Test if it's a file or directory ?
-	return BEntry(filename.c_str()).Exists();
-}
-
 std::string const
 file_type(const std::string& fullpath)
 {
@@ -92,15 +40,6 @@ file_type(const std::string& fullpath)
 			return "rust";
 	}
 	return "";
-}
-
-int
-get_year()
-{
-	// Get year
-	time_t time = ::time(NULL);
-	struct tm* tm = localtime(&time);
-	return tm->tm_year + 1900;
 }
 
 } // end of namespace

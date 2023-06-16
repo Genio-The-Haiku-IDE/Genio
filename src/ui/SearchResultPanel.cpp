f9848a7d4871bb2b8c6ffbe5d2bcbcd11a72897f
Freaxed <andrea.anzani@gmail.com>
2023-06-16 09:29:26 +0200

    improved readability: removed project path and autoexpand result

--- src/ui/SearchResultPanel.cpp
+++ src/ui/SearchResultPanel.cpp
@@ -43,13 +43,18 @@ SearchResultPanel::SearchResultPanel(): BColumnListView(ProblemLabel,
 								200.0, 20.0, 2000.0, 0), kLocationColumn);
 	AddColumn(new BStringColumn(B_TRANSLATE("Message"),
 								80.0, 80.0, 80.0, 0), kPositionColumn);
+								
+	//SetFont(be_fixed_font);
 }
 
 void
-SearchResultPanel::StartSearch(BString command)
+SearchResultPanel::StartSearch(BString command, BString projectPath)
 {
 	if (fGrepThread)
 		return;
+	fProjectPath = projectPath;
+	if (!fProjectPath.EndsWith("/"))
+		fProjectPath.Append("/");
 	ClearSearch();
 	BMessage message;
 	message.AddString("cmd", command);
@@ -71,55 +76,27 @@ void
 SearchResultPanel::MessageReceived(BMessage* msg)
 {
 	switch(msg->what) {
-		case MSG_REPORT_RESULT: {
-			RangeRow* parent = new RangeRow();
-			parent->fRange.MakeEmpty();
-			parent->SetField(new BStringField(msg->GetString("filename", "")), kLocationColumn);
-			AddRow(parent);
-			int32 c=0;
-			BMessage line;
-			while (msg->FindMessage("line",c++, &line) == B_OK) {
-				RangeRow* row = new RangeRow();
-				row->fRange = line;
-				row->SetField(new BStringField(line.GetString("text", "")), kLocationColumn);
-				row->SetField(new BStringField ("?"), kPositionColumn);
-				AddRow(row, parent);
-			}
-			msg->PrintToStream();
-		}
+		case MSG_REPORT_RESULT:
+			UpdateSearch(msg);
+		break;
 		case SEARCHRESULT_CLICK:{
 			RangeRow* range = dynamic_cast<RangeRow*>(CurrentSelection());
-			if (range) {
-				if (range->fRange.what == B_REFS_RECEIVED)
-					Window()->PostMessage(&range->fRange);
+			if (range && range->fRange.what == B_REFS_RECEIVED) {
+				Window()->PostMessage(&range->fRange);
 			}
 		}
 		break;
-		default:
-			BColumnListView::MessageReceived(msg);
-	};/*
-	if (msg->what == SEARCHRESULT_CLICK) {
-		RangeRow* range = dynamic_cast<RangeRow*>(CurrentSelection());
-		if (range) {
-			//range->fRange.PrintToStream();
-			BMessage start;
-			if (range->fRange.FindMessage("start", &start) == B_OK) {
-				const int32 be_line = start.GetDouble("line", -1.0);
-				const int32 lsp_char = start.GetDouble("character", -1.0);
-				entry_ref ref;
-				range->fRange.FindRef("ref", &ref);
-
-				BMessage refs(B_REFS_RECEIVED);
-				refs.AddInt32("be:line", be_line + 1);
-				refs.AddInt32("lsp:character", lsp_char);
-				refs.AddRef("refs", &ref);
-				//refs.PrintToStream();
-				Window()->PostMessage(&refs);
+		case MSG_GREP_DONE: {
+			if (fGrepThread) {
+				fGrepThread->InterruptExternal();
+				fGrepThread->Kill();
+				delete fGrepThread;
+				fGrepThread = nullptr;
 			}
 		}
-		return;
-	}*/
-
+		default:
+			BColumnListView::MessageReceived(msg);
+	};
 }
 
 void
@@ -131,24 +108,29 @@ SearchResultPanel::ClearSearch()
 void
 SearchResultPanel::UpdateSearch(BMessage* msg)
 {
-/*
-	BMessage dia;
-	int32 index = 0;
-	entry_ref ref;
-	if (msg->FindRef("ref", &ref) != B_OK)
+	BString filename = msg->GetString("filename", "");
+	filename.RemoveFirst(fProjectPath);
+	if (filename.IsEmpty())
 		return;
-	while (msg->FindMessage(std::to_string(index++).c_str(), &dia) == B_OK) {
-		RangeRow* row = new RangeRow();
-		dia.FindMessage("range", &row->fRange);
-		row->fRange.AddRef("ref", &ref);
-		//dia.PrintToStream();
-		row->SetField(new BStringField(dia.GetString("category","")), kLocationColumn);
-		row->SetField(new BStringField(dia.GetString("message","")), kPositionColumn);
-		row->SetField(new BStringField(dia.GetString("source","")), kSourceColumn);
-		AddRow(row);
-	}*/
+	RangeRow* parent = new RangeRow();
+	parent->fRange.MakeEmpty();
 
-	printf("FIX ME\n");
+	parent->SetField(new BStringField(filename), kLocationColumn);
+	AddRow(parent);
+	int32 c = 0;
+	BMessage line;
+	BString text;
+	while (msg->FindMessage("line", c++, &line) == B_OK) {
+		if (line.FindString("text", &text) == B_OK) {
+			RangeRow* row = new RangeRow();
+			row->fRange = line;
+			row->SetField(new BStringField(text), kLocationColumn);
+			AddRow(row, parent);
+			if (c == 1)
+				ExpandOrCollapse(parent, true);
+		}
+	}
+	msg->PrintToStream();
 }
 
 

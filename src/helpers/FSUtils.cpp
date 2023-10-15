1375baeb8360316ea643a5e9dcf61a06818560ef
Jackburton79 <stefano.ceccherini@gmail.com>
2023-10-15 14:42:29 +0200

    Made FSChmod able to handle also non-recursive case.
    Used it in Templatemanager instead of chmod() (deprecated).
    Cleanups.

--- src/helpers/FSUtils.cpp
+++ src/helpers/FSUtils.cpp
@@ -20,23 +20,23 @@ status_t FSCheckCopiable(BEntry *src,BEntry *dest)
 	// Checks to see if we can copy the src to dest.
 	if (!src || !dest)
 		return B_ERROR;
-	
+
 	if (!src->Exists())
 		return B_ENTRY_NOT_FOUND;
 
 	// Ensure that when we want the destination directory, that is exactly
 	// what we're working with. If we've been given an entry which is a file,
 	// extract the destination directory.
-	BEntry destdir;		
+	BEntry destdir;
 	if (dest->IsDirectory())
 		destdir = *dest;
 	else
 		dest->GetParent(&destdir);
-	
+
 	// check existence of target directory
 	if (!destdir.Exists())
 		return B_NAME_NOT_FOUND;
-		
+
 	// check space
 	entry_ref ref;
 	off_t src_bytes;
@@ -47,7 +47,7 @@ status_t FSCheckCopiable(BEntry *src,BEntry *dest)
 
 	if (src_bytes>dvolume.FreeBytes())
 		return B_DEVICE_FULL;
-	
+
 	// check permissions
 	if (dvolume.IsReadOnly())
 		return B_READ_ONLY;
@@ -68,7 +68,7 @@ status_t FSCheckCopiable(BEntry *src,BEntry *dest)
 		newpath = "The file ";
 		newpath += name;
 		newpath += " exists. Do you want to replace it?";
-		
+
 		BAlert *alert = new BAlert("Error", newpath.String(), "Replace File", "Skip File", "Stop");
 		status_t returncode = alert->Go();
 		switch (returncode) {
@@ -107,7 +107,7 @@ FSCopyFile(BEntry *src, BEntry *dest, bool clobber)
 			return status;
 		}
 	}
-	
+
 	BFile *srcfile = new BFile(src, B_READ_ONLY);
 	status = srcfile->InitCheck();
 	if (status != B_OK) {
@@ -115,10 +115,10 @@ FSCopyFile(BEntry *src, BEntry *dest, bool clobber)
 		delete destfile;
 		return status;
 	}
-	
+
 	char *buffer = new char[COPY_BUFFER_SIZE];
 	ssize_t bytes_read = 0;
-	
+
 	do {
 		bytes_read = srcfile->Read((void*)buffer, COPY_BUFFER_SIZE);
 		destfile->Write(buffer, bytes_read);
@@ -130,29 +130,29 @@ FSCopyFile(BEntry *src, BEntry *dest, bool clobber)
 	// copy attributes
 	BNode srcnode(src);
 	BNode destnode(dest);
-	
+
 	srcnode.RewindAttrs();
 
 	int8 *attr_buffer;
 	ssize_t attr_size;
 	attr_info attr_info;
 	char attr_name[B_ATTR_NAME_LENGTH];
-	
+
 	while (srcnode.GetNextAttrName(attr_name) == B_OK) {
 		// first get the size & type of the attribute
 		if (srcnode.GetAttrInfo(attr_name, &attr_info) != B_OK)
 			continue;
-		
+
 		// allocate memory to hold the attribute
-		attr_buffer = new int8[attr_info.size]; 
+		attr_buffer = new int8[attr_info.size];
 		if (attr_buffer == NULL)
 			continue;
-		
+
 		attr_size = srcnode.ReadAttr(attr_name, attr_info.type, 0LL, attr_buffer, attr_info.size);
-		
+
 		destnode.WriteAttr(attr_name, attr_info.type, 0LL, attr_buffer, attr_size);
 		destnode.Sync();
-												
+
 		delete[] attr_buffer;
 	}
 
@@ -165,11 +165,11 @@ FSMoveFile(BEntry *src, BEntry *dest, bool clobber)
 {
 	if (!src || !dest)
 		return B_ERROR;
-	
+
 	// Obtain the destination directory
 	BPath path;
 	dest->GetPath(&path);
-	
+
 	BPath parent;
 	path.GetParent(&parent);
 
@@ -177,14 +177,14 @@ FSMoveFile(BEntry *src, BEntry *dest, bool clobber)
 	status_t status = dir.SetTo(parent.Path());
 	if (status != B_OK)
 		return status;
-	
+
 	BString newpath = parent.Path();
 	char newname[B_FILE_NAME_LENGTH];
 	dest->GetName(newname);
 	newpath += "/";
 	newpath += newname;
 	status = src->MoveTo(&dir, newpath.String(), clobber);
-	
+
 	if (status == B_CROSS_DEVICE_LINK) {
 		BPath srcpath;
 		src->GetPath(&srcpath);
@@ -205,13 +205,16 @@ FSMoveFile(BEntry *src, BEntry *dest, bool clobber)
 namespace fs = std::filesystem;
 
 
-void
-FSChmodr(const fs::path& path, fs::perms perm)
+status_t
+FSChmod(const fs::path& path, fs::perms perm, bool recurse)
 {
-    fs::permissions(path, perm);      // set permissions on the top directory
-    for (auto& de : fs::recursive_directory_iterator(path)) {
-        fs::permissions(de, perm);    // set permissions
-    }
+    fs::permissions(path, perm); // set permissions on the top directory
+    if (recurse) {
+		for (auto& de : fs::recursive_directory_iterator(path)) {
+			fs::permissions(de, perm); // set permissions
+		}
+	}
+	return B_OK;
 }
 
 
@@ -231,11 +234,11 @@ FSDeleteFolder(BEntry *dirEntry)
 		if (status != B_OK)
 			break;
 	}
-	
+
 	if (status == B_OK) {
 		// delete top directory
 		status = dirEntry->Remove();
 	}
-	
+
 	return status;
 }
--- src/templates/TemplateManager.cpp
+++ src/templates/TemplateManager.cpp
@@ -36,52 +36,52 @@ TemplateManager::~TemplateManager()
 
 status_t
 TemplateManager::CopyFileTemplate(const entry_ref* source, const entry_ref* destination)
-{	
+{
 	status_t status = B_NOT_INITIALIZED;
-	
+
 	// Copy template file to destination
 	BPath sourcePath(source);
 	Entry sourceEntry(sourcePath.Path());
-	
+
 	BPath destPath(destination);
 	destPath.Append(source->name);
 	Entry destEntry(destPath.Path());
-	
+
 	status = BCopyEngine().CopyEntry(sourceEntry, destEntry);
 	if (status != B_OK) {
 		BString err(strerror(status));
 		LogError("Error creating new file %s in %s: %s", sourcePath.Path(), destPath.Path(),err.String());
 	}
 
-	chmod(destPath.Path(), 0777);
-	
+	FSChmod(destPath.Path(), fs::perms::all);
+
 	return status;
 }
 
 
 status_t
-TemplateManager::CopyProjectTemplate(const entry_ref* source, const entry_ref* destination, 
+TemplateManager::CopyProjectTemplate(const entry_ref* source, const entry_ref* destination,
 										const char* name)
-{	
+{
 	status_t status = B_NOT_INITIALIZED;
-	
+
 	BPath sourcePath(source);
 	Entry sourceEntry(sourcePath.Path());
-	
+
 	BPath destPath(destination);
 	destPath.Append(name);
 	Entry destEntry(destPath.Path());
-	
+
 	status = BCopyEngine(BCopyEngine::COPY_RECURSIVELY).CopyEntry(sourceEntry, destEntry);
 	if (status != B_OK) {
 		BString err(strerror(status));
 		LogError("Error creating new template %s in %s: %s", sourcePath.Path(), destPath.Path(),err.String());
 	}
-	
+
 	// TODO: Default templates in a tipical HPKG installation are readonly
 	// we are setting all permissions recursively here
-	FSChmodr(destPath.Path(), fs::perms::all);
-	
+	FSChmod(destPath.Path(), fs::perms::all, true);
+
 	return status;
 }
 
@@ -143,13 +143,13 @@ TemplateManager::GetUserTemplateDirectory()
 	// User template directory
 	BString retString;
 	BPath userPath;
-	
+
 	status_t status = find_directory (B_USER_SETTINGS_DIRECTORY, &userPath, true);
 	if (status == B_OK) {
 		userPath.Append(GenioNames::kApplicationName);
 		userPath.Append(kTemplateDirectory);
 		mkdir(userPath.Path(), 0777);
-		retString = userPath.Path();	
+		retString = userPath.Path();
 	}
 	return retString;
 }

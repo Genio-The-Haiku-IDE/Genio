86a2eebe4573448ff085f06dfd4d524791a5c943
Jackburton79 <stefano.ceccherini@gmail.com>
2023-10-24 12:01:49 +0200

    Import new settings files

---
+++ src/config/ConfigManager.h
@@ -0,0 +1,96 @@
+#pragma once
+#include "GMessage.h"
+#include <Autolock.h>
+class BView;
+
+
+class ConfigManagerReturn {
+public:
+		ConfigManagerReturn(GMessage& msg, const char* key, BLocker& lock):
+			fMsg(msg),
+			fKey(key),
+			fLocker(lock){
+		}
+
+		template< typename Return >
+        operator Return() { BAutolock lock(fLocker); return fMsg[fKey]; };
+private:
+	GMessage& fMsg;
+	const char* fKey;
+	BLocker& fLocker;
+};
+
+class ConfigManager {
+
+public:
+		explicit ConfigManager(){
+			fLocker.InitCheck();
+		}
+
+		void UpdateValue(GMessage&newValue) {
+			BAutolock lock(fLocker);
+			storage[newValue["key"]] = newValue["value"];
+			printf("Updated: "); newValue.PrintToStream();
+		}
+
+		template<typename T>
+		void AddConfig(const char* group, const char* key, const char* label, T default_value, GMessage* cfg = nullptr) {
+			GMessage configKey;
+			if (cfg)
+				configKey = *cfg;
+
+			configKey["group"]			= group;
+			configKey["key"]			= key;
+			configKey["label"]    		= label;
+			configKey["default_value"]  = default_value;
+			configKey["type_code"] 		= MessageValue<T>::Type();
+
+			configuration.AddMessage("config", &configKey);
+		}
+
+
+		void ResetToDefault() {
+			GMessage msg;
+			int i=0;
+			while(configuration.FindMessage("config", i++, &msg) == B_OK) {
+				storage[msg["key"]] = msg["default_value"];
+			}
+		}
+
+		void Print() {
+			storage.PrintToStream();
+			configuration.PrintToStream();
+		}
+
+		auto operator[](const char* key) -> ConfigManagerReturn {
+			type_code type;
+			if (storage.GetInfo(key, &type) != B_OK) {
+				printf("No info for key [%s]\n", key);
+				throw new std::exception();
+			}
+			return ConfigManagerReturn(storage, key, fLocker);
+		}
+
+		bool Has(GMessage& msg, const char* key) {
+			type_code type;
+			return (msg.GetInfo(key, &type) == B_OK);
+		}
+
+		GMessage&	Configuration() { return configuration; }
+
+		BView* MakeView();
+		//BView* MakeView2();
+		BView* MakeViewFor(const char* groupName, GMessage& config);
+		BView* MakeSelfHostingViewFor(GMessage& config);
+		BView* MakeViewFor(GMessage& config);
+
+
+protected:
+		GMessage storage;
+		GMessage configuration;
+		BLocker	 fLocker;
+
+
+};
+
+
---
+++ src/config/ConfigWindow.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023, Andrea Anzani 
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef ConfigWindow_H
+#define ConfigWindow_H
+#include "GMessage.h"
+#include <OutlineListView.h>
+#include <Box.h>
+#include <LayoutBuilder.h>
+#include <Button.h>
+#include <Window.h>
+
+class ConfigManager;
+
+class ConfigWindow : public BWindow {
+public:
+		ConfigWindow(ConfigManager& configManager);
+
+		void MessageReceived(BMessage* message);
+
+		BView* MakeViewFor(const char* groupName, GMessage& config);
+		BView* MakeSelfHostingViewFor(GMessage& config);
+		BView* MakeViewFor(GMessage& config);
+
+private:
+
+	ConfigManager& fConfigManager;
+
+	BView*	_Init();
+	void _PopulateListView();
+
+	BOutlineListView* 	fGroupList;
+	BButton* 	fButton;
+	BCardView* 	fCardView;
+
+};
+
+
+#endif // ConfigWindow_H

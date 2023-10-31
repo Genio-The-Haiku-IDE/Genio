9986a734079ec99879d2bc0a96d502928b377aed
D. Alfano (Nexus6) <nexus6.haiku@icloud.com>
2023-10-31 21:16:43 +0100

    Added user credentials when opening remote projects

---
+++ src/ui/GitCredentialsWindow.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018, Hrishikesh Hiraskar <hrishihiraskar@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+#ifndef _CREDENTIALS_WINDOW_H_
+#define _CREDENTIALS_WINDOW_H_
+
+#include <posix/sys/time.h>
+
+#include <InterfaceKit.h>
+
+enum {
+	kCredOK,
+	kCredCancel
+};
+
+/**
+ * The Credentials Window class.
+ */
+class GitCredentialsWindow : public BWindow {
+	/**
+	 * The text control for username.
+	 */
+	BTextControl* 			fUsername;
+	/**
+	 * The text control for password.
+	 */
+	BTextControl* 			fPassword;
+	/**
+	 * The username pointer.
+	 */
+	char*					fUsernamePtr;
+	/**
+	 * The password pointer.
+	 */
+	char*					fPasswordPtr;
+	public:
+							GitCredentialsWindow(char*, char*);
+	virtual void			MessageReceived(BMessage*);
+};
+
+#endif

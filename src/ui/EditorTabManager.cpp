48b8edae8f49ed4b77b9ffda0f280be6b817c9ea
Freaxed <andrea.anzani@gmail.com>
2023-11-22 17:58:58 +0100

    TabManager can search by entry_ref

--- src/ui/EditorTabManager.cpp
+++ src/ui/EditorTabManager.cpp
@@ -6,23 +6,51 @@
 
 #include "EditorTabManager.h"
 #include "Editor.h"
+#include "Log.h"
 
 EditorTabManager::EditorTabManager(const BMessenger& target) : TabManager(target)
 {
 }
 
-Editor*		
+Editor*
 EditorTabManager::EditorAt(int32 index)
 {
 	return dynamic_cast<Editor*>(ViewForTab(index));
 }
 
-Editor*		
+Editor*
 EditorTabManager::SelectedEditor()
 {
 	int32 sel = SelectedTabIndex();
 	if (sel >= 0 && sel < CountTabs())
 		return EditorAt(sel);
-	
-	return NULL;
+
+	return nullptr;
+}
+
+Editor*
+EditorTabManager::EditorBy(entry_ref* ref)
+{
+	BEntry entry(ref, true);
+	int32 filesCount = CountTabs();
+
+
+	for (int32 index = 0; index < filesCount; index++) {
+
+		Editor* editor = EditorAt(index);
+
+		if (editor == nullptr) {
+			BString notification;
+			notification
+				<< "Index " << index << ": NULL editor pointer";
+			LogInfo(notification.String());
+			continue;
+		}
+
+		BEntry matchEntry(editor->FileRef(), true);
+
+		if (matchEntry == entry)
+			return editor;
+	}
+	return nullptr;
 }

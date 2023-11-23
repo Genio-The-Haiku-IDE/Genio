a2e21c93e6356c05dbd36979e9114f3f17fd6b62
Nexus6 <nexus6@genio.local>
2023-11-23 22:08:09 +0100

    Implement a base template class GAlert to create generic BAlert-like requesters

---
+++ src/alert/GTextAlert.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 Nexus6 
+ */
+
+#include "GTextAlert.h"
+
+#include <Application.h>
+#include <Button.h>
+#include <Catalog.h>
+#include <CheckBox.h>
+#include <LayoutBuilder.h>
+#include <ScrollView.h>
+#include <StringView.h>
+
+#include <string>
+#include <vector>
+
+#include "Utils.h"
+
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "GTextAlert"
+
+GTextAlert::GTextAlert(const char *title, const char *message, const char *text)
+	:
+	GAlert<BString>(title, message),
+	fTextControl(nullptr),
+	fText(text)
+{
+	_InitInterface();
+	CenterOnScreen();
+}
+
+
+GTextAlert::~GTextAlert()
+{
+}
+
+
+void
+GTextAlert::_InitInterface()
+{
+	fTextControl = new BTextControl("", fText, new BMessage(kTextControlMessage));
+	// BLayoutBuilder::Group<>(GetPlaceholderView(), B_VERTICAL, B_USE_DEFAULT_SPACING)
+		// .Add(fTextControl)
+		// .End();
+	GetPlaceholderView()->AddChild(fTextControl);
+}
+
+
+void
+GTextAlert::Show()
+{
+	GAlert::Show();
+}
+
+
+void
+GTextAlert::MessageReceived(BMessage* message)
+{
+	switch (message->what) {
+		case 1: {
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+
+	GAlert::MessageReceived(message);
+}

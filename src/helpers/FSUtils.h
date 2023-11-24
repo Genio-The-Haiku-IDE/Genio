476ee913426bd7d819d64c7518e6137a80b54fb7
Jackburton79 <stefano.ceccherini@gmail.com>
2023-11-24 21:39:13 +0100

    FSUtils: Headers cleanup

--- src/helpers/FSUtils.h
+++ src/helpers/FSUtils.h
@@ -1,16 +1,19 @@
 #ifndef FSUTILS_H_
 #define FSUTILS_H_
 
-#include <Errors.h>
+#include <SupportDefs.h>
+
 #include <filesystem>
 
 #define FS_CLOBBER 'fscl'
 #define FS_SKIP 'fssk'
 
+class BEntry;
+
 namespace fs = std::filesystem;
 status_t FSCheckCopiable(BEntry *src, BEntry *dest);
-status_t FSCopyFile(BEntry *src,BEntry *dest, bool clobber);
-status_t FSMoveFile(BEntry *src,BEntry *dest, bool clobber);
+status_t FSCopyFile(BEntry *src, BEntry *dest, bool clobber);
+status_t FSMoveFile(BEntry *src, BEntry *dest, bool clobber);
 status_t FSChmod(const fs::path& path, fs::perms perm, bool recurse = false);
 status_t FSDeleteFolder(BEntry *dirEntry);
 

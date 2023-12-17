8f7eb0b6721f4b6d62cb03ca8bd9c0a9f2472179
Jackburton79 <stefano.ceccherini@gmail.com>
2023-12-17 14:18:56 +0100

    Moved static methods to be class members. Minor style cleanups.
    No functional changes

--- src/GenioApp.h
+++ src/GenioApp.h
@@ -1,35 +1,37 @@
 /*
  * Copyright 2017 A. Mosca 
+ * Copyright 2023, The Genio Team
  * All rights reserved. Distributed under the terms of the MIT license.
  */
-#ifndef GenioAPP_H
-#define GenioAPP_H
+
+#pragma once
 
 #include <Application.h>
 #include <Path.h>
+#include <StringList.h>
 
 class ConfigManager;
 class GenioWindow;
 class GenioApp : public BApplication {
 public:
-								GenioApp();
-	virtual						~GenioApp();
+							GenioApp();
+	virtual					~GenioApp();
 
-	virtual	void				AboutRequested();
-	virtual	void				ArgvReceived(int32 agrc, char** argv);
-	virtual	void				MessageReceived(BMessage* message);
-	virtual	bool				QuitRequested();
-	virtual	void				ReadyToRun();
-	virtual	void				RefsReceived(BMessage* message);
-	void						PrepareConfig(ConfigManager& cfg);
+	virtual	void			AboutRequested();
+	virtual	void			ArgvReceived(int32 agrc, char** argv);
+	virtual	void			MessageReceived(BMessage* message);
+	virtual	bool			QuitRequested();
+	virtual	void			ReadyToRun();
+	virtual	void			RefsReceived(BMessage* message);
 
 private:
-			void				_CheckSettingsVersion();
-private:
-		GenioWindow*			fGenioWindow;
-		BPath					fConfigurationPath;
+	static BStringList		_SplitChangeLog(const char* changeLog);
+	static void				_SetSessionLogLevel(char level);
+	int						_HandleArgs(int argc, char **argv);
+	static void				_PrepareConfig(ConfigManager& cfg);
+
+	GenioWindow*			fGenioWindow;
+	BPath					fConfigurationPath;
 };
 
 extern ConfigManager gCFG;
-
-#endif //GenioAPP_H

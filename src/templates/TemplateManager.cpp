4a72bee6551d0d364a9167d199673699cbb7d4ee
Nexus6 <nexus6@genio.local>
2024-01-04 00:58:06 +0100

    Fix a crash that occurs when creating a new file from a template

--- src/templates/TemplateManager.cpp
+++ src/templates/TemplateManager.cpp
@@ -51,9 +51,9 @@ TemplateManager::CopyFileTemplate(const entry_ref* source, const entry_ref* dest
 	if (status != B_OK) {
 		BString err(strerror(status));
 		LogError("Error creating new file %s in %s: %s", sourcePath.Path(), destPath.Path(),err.String());
+	} else {
+		FSChmod(destPath.Path(), fs::perms::all);
 	}
-
-	FSChmod(destPath.Path(), fs::perms::all);
 
 	return status;
 }
@@ -74,11 +74,11 @@ TemplateManager::CopyProjectTemplate(const entry_ref* source, const entry_ref* d
 	if (status != B_OK) {
 		BString err(strerror(status));
 		LogError("Error creating new template %s in %s: %s", sourcePath.Path(), destPath.Path(),err.String());
+	} else {
+		// TODO: Default templates in a tipical HPKG installation are readonly
+		// we are setting all permissions recursively here
+		FSChmod(destPath.Path(), fs::perms::all, true);
 	}
-
-	// TODO: Default templates in a tipical HPKG installation are readonly
-	// we are setting all permissions recursively here
-	FSChmod(destPath.Path(), fs::perms::all, true);
 
 	return status;
 }

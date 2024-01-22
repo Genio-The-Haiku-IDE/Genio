55ebd8b01cd51804227ecf143df00ccbd18e82ac
Freaxed <andrea.anzani@gmail.com>
2024-01-22 09:17:12 +0100

    ConfigManager: introduced the PermanentStorageProvider

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -13,6 +13,123 @@
 
 #include "Log.h"
 
+class PermanentStorageProvider {
+public:
+	enum kPSPMode { kPSPReadMode, kPSPWriteMode };
+
+						PermanentStorageProvider(BPath destination, kPSPMode mode) {};
+
+	virtual status_t	InitCheck() = 0;
+	virtual status_t	LoadKey(ConfigManager& manager, const char* key, GMessage& storage, GMessage& parConfig) = 0;
+	virtual status_t	SaveKey(ConfigManager& manager, const char* key, GMessage& storage) = 0;
+
+};
+
+class BMessagePSP : public PermanentStorageProvider {
+public:
+		BMessagePSP(BPath dest, kPSPMode mode): PermanentStorageProvider(dest, mode){
+			status = file.SetTo(dest.Path(), mode == PermanentStorageProvider::kPSPReadMode ? B_READ_ONLY : (B_WRITE_ONLY | B_CREATE_FILE));
+			if (status == B_OK && file.IsReadable()) {
+				status = fromFile.Unflatten(&file);
+			}
+		}
+		~BMessagePSP() {
+			if (status == B_OK && file.IsWritable()) {
+				fromFile.Flatten(&file);
+				file.Flush();
+			}
+		}
+
+		status_t	InitCheck() {
+			return status;
+		}
+
+		status_t	LoadKey(ConfigManager& manager, const char* key, GMessage& storage, GMessage& paramerConfig) {
+			if (fromFile.Has(key) && _SameTypeAndFixedSize(&fromFile, key, &storage, key)) {
+				manager[key] = fromFile[key];
+				return B_OK;
+			}
+			return B_NAME_NOT_FOUND;
+		}
+		status_t	SaveKey(ConfigManager& manager, const char* key, GMessage& storage) {
+				fromFile[key] = storage[key];
+				return B_OK;
+		}
+
+	private:
+		BFile file;
+		GMessage fromFile;
+		status_t status = B_ERROR;
+
+		bool _SameTypeAndFixedSize(
+			BMessage* msgL, const char* keyL, BMessage* msgR, const char* keyR) const
+		{
+			type_code typeL = 0;
+			bool fixedSizeL = false;
+			if (msgL->GetInfo(keyL, &typeL, &fixedSizeL) == B_OK) {
+				type_code typeR = 0;
+				bool fixedSizeR = false;
+				if (msgR->GetInfo(keyR, &typeR, &fixedSizeR) == B_OK) {
+					return (typeL == typeR && fixedSizeL == fixedSizeR);
+				}
+			}
+			return false;
+		}
+};
+
+class AttributePSP : public PermanentStorageProvider {
+public:
+		AttributePSP(BPath attributeFilePath, kPSPMode mode): PermanentStorageProvider(attributeFilePath, mode){
+			status = nodeAttr.SetTo(attributeFilePath.Path());
+		}
+
+	status_t	InitCheck() { return status; }
+	status_t	LoadKey(ConfigManager& manager, const char* key, GMessage& storage, GMessage& paramerConfig)
+	{
+		BString attrName("genio:");
+		attrName.Append(key);
+		const void* data = nullptr;
+		ssize_t numBytes = 0;
+		type_code type = paramerConfig.Type("default_value");
+		if (paramerConfig.FindData("default_value", type, &data, &numBytes) == B_OK) {
+			void* buffer = malloc(numBytes);
+			ssize_t readStatus = nodeAttr.ReadAttr(attrName.String(), type, 0, buffer, numBytes);
+			if (readStatus <=0)
+				return B_NAME_NOT_FOUND;
+
+			storage.RemoveName(key);
+			status_t st = storage.AddData(key, type, buffer, numBytes);
+			if (st == B_OK) {
+				GMessage noticeMessage(manager.UpdateMessageWhat());
+				noticeMessage["key"] = key;
+				noticeMessage["value"] = storage[key];
+				if (be_app != nullptr)
+					be_app->SendNotices(manager.UpdateMessageWhat(), &noticeMessage);
+			}
+			free(buffer);
+			return st;
+		}
+		return B_NAME_NOT_FOUND;
+	}
+	status_t	SaveKey(ConfigManager& manager, const char* key, GMessage& storage) {
+			// save as attribute:
+			BString attrName("genio:");
+			attrName.Append(key);
+			const void* data = nullptr;
+			ssize_t numBytes = 0;
+			type_code type = storage.Type(key);
+			if (storage.FindData(key, type, &data, &numBytes) == B_OK) {
+				if (nodeAttr.WriteAttr(attrName.String(), type, 0, data, numBytes) <= 0) {
+					return B_ERROR;
+				}
+			}
+			return B_OK;
+	}
+private:
+	BNode	nodeAttr;
+	status_t status = B_ERROR;
+};
+
 
 ConfigManager::ConfigManager(const int32 messageWhat)
 	:
@@ -53,139 +170,51 @@ ConfigManager::Has(GMessage& msg, const char* key) const
 status_t
 ConfigManager::LoadFromFile(BPath messageFilePath, BPath attributeFilePath)
 {
-	GMessage fromFile;
-	BFile file;
-	BNode* nodeAttr = nullptr;
-	status_t status = file.SetTo(messageFilePath.Path(), B_READ_ONLY);
-	if (status == B_OK) {
-		status = fromFile.Unflatten(&file);
+	status_t status = B_OK;
+	BMessagePSP storageM(messageFilePath, PermanentStorageProvider::kPSPReadMode);
+	AttributePSP storageA(attributeFilePath, PermanentStorageProvider::kPSPReadMode);
+	GMessage msg;
+	int i = 0;
+	while (configuration.FindMessage("config", i++, &msg) == B_OK) {
+		const char* key = msg["key"];
+		StorageType storageType = (StorageType)((int32)msg["storage_type"]);
+		status_t status = B_ERROR;
+		if (storageType == kStorageTypeBMessage && storageM.InitCheck() == B_OK) {
+			status = storageM.LoadKey(*this, key, storage, msg);
+		} else if (storageType == kStorageTypeAttribute && storageA.InitCheck() == B_OK) {
+			status = storageA.LoadKey(*this, key, storage, msg);
+		}
 		if (status == B_OK) {
-			GMessage msg;
-			int i = 0;
-			while (configuration.FindMessage("config", i++, &msg) == B_OK) {
-				const char* key = msg["key"];
-				if ((bool)msg["as_attribute"] == false) {
-					if (fromFile.Has(key) && _SameTypeAndFixedSize(&fromFile, key, &storage, key)) {
-						(*this)[key] = fromFile[key];
-						LogInfo("Configuration files loading value for key [%s]", (const char*)msg["key"]);
-					} else {
-						LogError("Configuration files does not contain the vaid key [%s]", (const char*)msg["key"]);
-					}
-				} else {
-					if (attributeFilePath == BPath()) {
-						LogError("Can't load a config file attribute. No path specified!");
-						continue;
-					}
-					if (nodeAttr == nullptr) {
-						nodeAttr = new BNode();
-						status_t statusFile = nodeAttr->SetTo(attributeFilePath.Path());
-						if (statusFile != B_OK) {
-							LogErrorF("file error (%s) on loading attribute %s", strerror(statusFile), key);
-							delete nodeAttr;
-							nodeAttr = nullptr;
-							continue;
-						}
-					}
-
-					// save as attribute:
-					BString attrName("genio:");
-					attrName.Append(key);
-					const void* data = nullptr;
-					ssize_t numBytes = 0;
-					type_code type = msg.Type("default_value");
-					if (msg.FindData("default_value", type, &data, &numBytes) == B_OK) {
-						void* buffer = malloc(numBytes);
-						ssize_t readStatus = nodeAttr->ReadAttr(attrName.String(), type, 0, buffer, numBytes);
-						if (readStatus == B_ENTRY_NOT_FOUND)
-							LogInfo("Attribute not found! [%s] on [%s]", attrName.String(), attributeFilePath.Path());
-						else if (readStatus <= 0) {
-							LogError("Can't load config from attribute: [%s] on [%s] (%d)", attrName.String(), attributeFilePath.Path(), readStatus);
-						} else {
-							storage.RemoveName(key);
-							if (storage.AddData(key, type, buffer, numBytes) == B_OK) {
-								GMessage noticeMessage(fWhat);
-								noticeMessage["key"]  	= key;
-								noticeMessage["value"]  = storage[key];
-								if (be_app != nullptr)
-									be_app->SendNotices(fWhat, &noticeMessage);
-							} else {
-								LogError("Can't add config from attribute: [%s] on [%s] (%d)", attrName.String(), attributeFilePath.Path(), readStatus);
-							}
-						}
-						free(buffer);
-					}
-				}
-			}
+			LogInfo("Config file: loaded value for key [%s] (StorageType %d)", key, storageType);
+		} else {
+			LogError("Config file: unable to get valid key [%s] (%s) (StorageType %d)", key, strerror(status), storageType);
 		}
 	}
-	storage.PrintToStream();
 	return status;
 }
-
-
-bool
-ConfigManager::_SameTypeAndFixedSize(BMessage* msgL, const char* keyL,
-									  BMessage* msgR, const char* keyR) const
-{
-	type_code typeL = 0;
-	bool fixedSizeL = false;
-	if (msgL->GetInfo(keyL, &typeL, &fixedSizeL) == B_OK) {
-		type_code typeR = 0;
-		bool fixedSizeR = false;
-		if (msgR->GetInfo(keyR, &typeR, &fixedSizeR) == B_OK) {
-			return (typeL == typeR && fixedSizeL == fixedSizeR);
-		}
-	}
-	return false;
-}
-
 
 status_t
 ConfigManager::SaveToFile(BPath messageFilePath, BPath attributeFilePath)
 {
-	BFile file;
-	BNode* fileAttr = nullptr;
-	status_t status = file.SetTo(messageFilePath.Path(), B_WRITE_ONLY | B_CREATE_FILE);
-	if (status == B_OK) {
-			GMessage outFile;
-			GMessage msg;
-			int i = 0;
-			while (configuration.FindMessage("config", i++, &msg) == B_OK) {
-				const char* key = msg["key"];
-
-				if ((bool)msg["as_attribute"] == false) {
-					outFile[key] = storage[key];
-				} else {
-
-					if (attributeFilePath == BPath()) {
-						LogError("Can't save a config file attribute. No path specified!");
-						continue;
-					}
-					if (fileAttr == nullptr) {
-						fileAttr = new BNode();
-						status_t statusFile = fileAttr->SetTo(attributeFilePath.Path());
-						if (statusFile != B_OK) {
-							LogErrorF("file error (%s) on saving attribute %s", strerror(statusFile), key);
-							delete fileAttr;
-							fileAttr = nullptr;
-							continue;
-						}
-					}
-
-					// save as attribute:
-					BString attrName("genio:");
-					attrName.Append(key);
-					const void* data = nullptr;
-					ssize_t numBytes = 0;
-					type_code type = storage.Type(key);
-					if (storage.FindData(key, type, &data, &numBytes) == B_OK) {
-						if (fileAttr->WriteAttr(attrName.String(), type, 0, data, numBytes) <= 0) {
-							LogError("Can't save config as attribute: %s\n", attrName.String());
-						}
-					}
-				}
-			}
-			status = outFile.Flatten(&file);
+	status_t status = B_OK;
+	BMessagePSP storageM(messageFilePath, PermanentStorageProvider::kPSPWriteMode);
+	AttributePSP storageA(attributeFilePath, PermanentStorageProvider::kPSPWriteMode);
+	GMessage msg;
+	int i = 0;
+	while (configuration.FindMessage("config", i++, &msg) == B_OK) {
+		const char* key = msg["key"];
+		status_t status = B_ERROR;
+		StorageType storageType = (StorageType)((int32)msg["storage_type"]);
+		if (storageType == kStorageTypeBMessage && storageM.InitCheck() == B_OK) {
+			status = storageM.SaveKey(*this, key, storage);
+		} else if (storageType == kStorageTypeAttribute && storageA.InitCheck() == B_OK) {
+			status = storageA.SaveKey(*this, key, storage);
+		}
+		if (status == B_OK) {
+			LogInfo("Config file: saved value for key [%s] (StorageType %d)", key, storageType);
+		} else {
+			LogError("Config file: unable to store valid key [%s] (%s) (StorageType %d)", key, strerror(status), storageType);
+		}
 	}
 	return status;
 }

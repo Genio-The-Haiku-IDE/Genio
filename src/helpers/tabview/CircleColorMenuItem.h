409e0791c29bc81c8ca0bcb7a5631bdd40e79966
Freaxed <andrea.anzani@gmail.com>
2024-02-08 19:29:08 +0100

    Added CircleColorMenuItem

---
+++ src/helpers/tabview/CircleColorMenuItem.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024, Andrea Anzani <andrea.anzani@gmail.com>
+ * Based on ColorMenuItem.h
+ *
+ * Copyright 2002-2006, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Mattias Sundblad
+ *		Andrew Bachmann
+ */
+#pragma once
+#include <MenuItem.h>
+
+class CircleColorMenuItem: public BMenuItem {
+public:
+								CircleColorMenuItem(const char* label, rgb_color color,
+									BMessage* message);
+			rgb_color&			Color() { return fItemColor; };
+	virtual void 				GetContentSize(float* width, float* height);
+
+protected:
+	virtual void				DrawContent();
+
+private:
+			rgb_color			fItemColor;
+};
+
+

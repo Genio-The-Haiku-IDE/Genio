Genio-The-Haiku-IDE/Genio

commit 564efbcfb5face819e2353f8ff5bc672272f4fd0

Makefile: simplifications Remove arch and platform since we aren't using them. Avoid using findpath Remove commented stuff Hope this helps with cross compiling

Makefile
@@ -1,8 +1,5 @@
 ## Genio - The Haiku IDE Makefile ##############################################
 
-arch := $(shell getarch)
-platform := $(shell uname -p)
-
 debug ?= 0
 ifeq ($(debug), 0)
 	DEBUGGER :=
@@ -27,7 +24,6 @@ ifeq ($(debug), 0)
 else
 	NAME := Genio_debug
 endif
-
 
 TARGET_DIR := app
 
@@ -93,7 +89,6 @@ SRCS += src/templates/TemplatesMenu.cpp
 SRCS += src/templates/TemplateManager.cpp
 SRCS += src/helpers/PipeImage.cpp
 
-
 RDEFS := Genio.rdef
 
 LIBS  = be shared translation localestub $(STDCPPLIBS)
@@ -102,18 +97,13 @@ LIBS += git2
 LIBS += src/scintilla/bin/libscintilla.a
 LIBS += yaml-cpp
 
-# LIBPATHS = $(shell findpaths -a $(platform) B_FIND_PATH_DEVELOP_LIB_DIRECTORY)
-# LIBPATHS  = /boot/home/config/non-packaged/lib
-# $(info LIBPATHS="$(LIBPATHS)")
-
-SYSTEM_INCLUDE_PATHS  = $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private)
-SYSTEM_INCLUDE_PATHS  = $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/interface)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/shared)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/storage)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/support)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/tracker)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/locale)
-SYSTEM_INCLUDE_PATHS += $(shell findpaths -a $(platform) -e B_FIND_PATH_HEADERS_DIRECTORY lexilla)
+SYSTEM_INCLUDE_PATHS  = $(BUILDHOME)/headers/private/interface
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/shared
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/storage
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/support
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/tracker
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/locale
+SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/lexilla
 SYSTEM_INCLUDE_PATHS += src/scintilla/haiku
 SYSTEM_INCLUDE_PATHS += src/scintilla/include
 
@@ -121,7 +111,6 @@ SYSTEM_INCLUDE_PATHS += src/scintilla/include
 SYSTEM_INCLUDE_PATHS += src/override
 
 CFLAGS += -Wall -Werror
-
 CXXFLAGS := -std=c++20 -fPIC
 
 #ifneq ($(BUILD_WITH_CLANG), 0)
@@ -140,14 +129,12 @@ $(OBJ_DIR)/%.o : %.cpp
 	$(CXX) -c $< $(INCLUDES) $(CFLAGS) $(CXXFLAGS) -o "$@"
 
 deps:
-#	$(MAKE) -C src/lexilla/src
 	$(MAKE) -C src/scintilla/haiku
 
 .PHONY: clean deps
 
 cleanall: clean
 	$(MAKE) clean -C src/scintilla/haiku
-	#$(MAKE) clean -C src/lexilla/src
 	rm -f txt2header
 	rm -f Changelog.h
 

commit 062c2743db773cebd0b299c4b79678aa19831226

Partly revert 564efbcfb5face819e2353f8ff5bc672272f4fd0 which broke compiling on 32 bit / x86

Makefile
@@ -1,5 +1,4 @@
 ## Genio - The Haiku IDE Makefile ##############################################
-
 debug ?= 0
 ifeq ($(debug), 0)
 	DEBUGGER :=
@@ -30,6 +29,9 @@ TARGET_DIR := app
 TYPE := APP
 
 APP_MIME_SIG := "application/x-vnd.Genio"
+
+arch := $(shell getarch)
+platform := $(shell uname -p)
 
 SRCS := src/GenioApp.cpp
 SRCS += src/alert/GTextAlert.cpp
@@ -98,13 +100,13 @@ LIBS += git2
 LIBS += src/scintilla/bin/libscintilla.a
 LIBS += yaml-cpp
 
-SYSTEM_INCLUDE_PATHS  = $(BUILDHOME)/headers/private/interface
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/shared
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/storage
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/support
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/tracker
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/private/locale
-SYSTEM_INCLUDE_PATHS += $(BUILDHOME)/headers/lexilla
+SYSTEM_INCLUDE_PATHS  = $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/interface)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/shared)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/storage)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/support)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/tracker)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -e B_FIND_PATH_HEADERS_DIRECTORY private/locale)
+SYSTEM_INCLUDE_PATHS += $(shell findpaths -a $(platform) -e B_FIND_PATH_HEADERS_DIRECTORY lexilla)
 SYSTEM_INCLUDE_PATHS += src/scintilla/haiku
 SYSTEM_INCLUDE_PATHS += src/scintilla/include
 
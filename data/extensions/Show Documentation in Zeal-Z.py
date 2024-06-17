2254a8818a85dfa917b53c3d8c75df7fc2bef591
Nexus6 <105149392+nexus6-haiku@users.noreply.github.com>
2024-06-18 00:45:45 +0200

    Enable a lightweight ExtensionManager (#411)
    * Enable a lightweight ExtensionManager
    
    * added haiku-format extension
    
    * Removed unused imports
    
    * fixed unused imports and minor style issues
    
    * Inform the user that the command is not available or fialed to launch
    
    * Removed comment
    
    ---------
    
    Co-authored-by: Freaxed <andrea.anzani@gmail.com>

---
+++ data/extensions/Show Documentation in Zeal-Z.py
@@ -0,0 +1,59 @@
+#!python3
+# Copyright 2024, The Genio Team
+# All rights reserved. Distributed under the terms of the MIT license.
+# Author: Nexus6 <nexus6@disroot.org>
+
+import subprocess
+from Be import BMessenger, BMessage, B_GET_PROPERTY
+
+genio = BMessenger("application/x-vnd.Genio")
+
+
+def GetSymbol():
+    message = BMessage(B_GET_PROPERTY)
+    message.AddSpecifier("Symbol")
+    message.AddSpecifier("SelectedEditor")
+    reply = BMessage()
+    genio.SendMessage(message, reply)
+    result = reply.GetString("result", "")
+    error = reply.GetInt32("error", 0)
+    return result, error
+
+
+def GetSelection():
+    message = BMessage(B_GET_PROPERTY)
+    message.AddSpecifier("Selection")
+    message.AddSpecifier("SelectedEditor")
+    reply = BMessage()
+    genio.SendMessage(message, reply)
+    result = reply.GetString("result", "")
+    error = reply.GetInt32("error", 0)
+    return result, error
+
+
+def OpenZeal(symbol):
+    run = -1
+    try:
+        result = subprocess.run(["Zeal", symbol])
+        run = result.returncode
+    except Exception:
+        print("Error executing Zeal.")
+    if run != 0:
+        print("Return code " + str(run))
+        subprocess.run(["alert", "--warning",
+                        "Could not run Zeal.\n"
+                        "Ensure the package is installed!"])
+
+
+def main():
+    selection, error = GetSelection()
+    if selection != "" and error == 0:
+        OpenZeal(selection)
+    else:
+        symbol, error = GetSymbol()
+        if symbol != "" and error == 0:
+            OpenZeal(symbol)
+
+
+if __name__ == "__main__":
+    main()

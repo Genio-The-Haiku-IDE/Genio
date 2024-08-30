c958e6a38bfe812fa0724b663a88c275bef93bc3
Jackburton79 <stefano.ceccherini@gmail.com>
2024-08-30 10:55:26 +0200

    Sort headers. Minor simplification

--- src/ui/EditorContextMenu.cpp
+++ src/ui/EditorContextMenu.cpp
@@ -6,30 +6,30 @@
 
 #include "EditorContextMenu.h"
 
-#include "Editor.h"
-#include "GenioWindowMessages.h"
-
 #include <Autolock.h>
 #include <Catalog.h>
-#include <PopUpMenu.h>
 #include <MenuItem.h>
+#include <PopUpMenu.h>
+
 #include "ActionManager.h"
-#include "LSPEditorWrapper.h"
-#include "GMessage.h"
+#include "Editor.h"
 #include "EditorMessages.h"
+#include "GenioWindowMessages.h"
+#include "GMessage.h"
+#include "LSPEditorWrapper.h"
 #include "ProjectBrowser.h"
 
 #undef  B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "GenioWindow"
 
-
 BPopUpMenu* EditorContextMenu::sMenu = nullptr;
 BPopUpMenu* EditorContextMenu::sFixMenu = nullptr;
 
+
 EditorContextMenu::EditorContextMenu()
 {
-
 }
+
 
 void
 EditorContextMenu::_CreateMenu()
@@ -71,7 +71,6 @@ EditorContextMenu::_CreateMenu()
 
 	refMessage = new BMessage(*refMessage);
 	ActionManager::AddItem(MSG_PROJECT_MENU_OPEN_TERMINAL, sMenu, refMessage);
-
 }
 
 
@@ -89,17 +88,17 @@ EditorContextMenu::Show(Editor* editor, BPoint point)
 	}
 
 	BPopUpMenu* menu = sMenu;
-	//NOTE: the target should always be editor (for all messages) but we need fist to move them
-	//from the window.
+	// NOTE: the target should always be editor (for all messages) but we need fist to move them
+	// from the window.
 	menu->SetTargetForItems((BHandler*)editor->Window());
 
 	LSPEditorWrapper* lsp = editor->GetLSPEditorWrapper();
-	if (lsp) {
+	if (lsp != nullptr) {
 		LSPDiagnostic dia;
 		BPoint p = editor->ConvertFromScreen(point);
 		Sci_Position sci_position = editor->SendMessage(SCI_POSITIONFROMPOINT, p.x, p.y);
 		int32 index = lsp->DiagnosticFromPosition(sci_position, dia);
-		if ( index > -1 && dia.diagnostic.codeActions.value().size() > 0) {
+		if (index > -1 && dia.diagnostic.codeActions.value().size() > 0) {
 			sFixMenu->RemoveItems(0, sFixMenu->CountItems(), true);
 			std::vector<CodeAction> actions = dia.diagnostic.codeActions.value();
 			for (int i = 0; i < static_cast<int>(actions.size()); i++) {
@@ -122,12 +121,10 @@ EditorContextMenu::Show(Editor* editor, BPoint point)
 	ActionManager::GetMessage(MSG_PROJECT_MENU_SHOW_IN_TRACKER, sMenu)->ReplaceRef("ref", editor->FileRef());
 	ActionManager::GetMessage(MSG_PROJECT_MENU_OPEN_TERMINAL, sMenu)->ReplaceRef("ref", editor->FileRef());
 
-
 	bool isFindInBrowserEnable = ActionManager::IsEnabled(MSG_FIND_IN_BROWSER);
 	ActionManager::SetEnabled(MSG_FIND_IN_BROWSER, (editor->GetProjectFolder() != nullptr));
 
-	menu->Go(BPoint(point.x, point.y), true);
+	menu->Go(point, true);
 
 	ActionManager::SetEnabled(MSG_FIND_IN_BROWSER, isFindInBrowserEnable);
-
 }

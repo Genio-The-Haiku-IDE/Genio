d8646226aa0952011f12cc2bbccb0a63f8811554
Jackburton79 <stefano.ceccherini@gmail.com>
2024-10-04 18:52:55 +0200

    When ticking build indicator animation, only invalidate project title items

--- src/ui/ProjectBrowser.cpp
+++ src/ui/ProjectBrowser.cpp
@@ -442,10 +442,12 @@ ProjectBrowser::MessageReceived(BMessage* message)
 		case kTick:
 		{
 			if (fIsBuilding) {
-				// TODO: Only invalidate the project item and
-				// Maybe move inside the ProjectOutlineListView
+				// TODO: Only invalidate the active project item ?
 				ProjectTitleItem::TickAnimation();
-				fOutlineListView->Invalidate();
+				for (int32 i = 0; i != fProjectProjectItemList.CountItems(); i++) {
+					int32 itemIndex = fOutlineListView->IndexOf(fProjectProjectItemList.ItemAt(i));
+					fOutlineListView->InvalidateItem(itemIndex);
+				}
 			}
 			break;
 		}

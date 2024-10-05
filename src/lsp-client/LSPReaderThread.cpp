cc1f7e63a1bae3edab1a61ec66ffacebc8cd87d6
Jackburton79 <stefano.ceccherini@gmail.com>
2024-10-05 17:23:36 +0200

    Cleanup. Removed debug leftover

--- src/lsp-client/LSPReaderThread.cpp
+++ src/lsp-client/LSPReaderThread.cpp
@@ -4,11 +4,13 @@
  */
 
 #include "LSPReaderThread.h"
+
 #include "LSPPipeClient.h"
 
 LSPReaderThread::LSPReaderThread(LSPPipeClient& looper)
 	:
-	GenericThread("LSPReaderThread"), fTransport(looper)
+	GenericThread("LSPReaderThread"),
+	fTransport(looper)
 {
 }
 
@@ -17,7 +19,6 @@ status_t
 LSPReaderThread::ExecuteUnit()
 {
 	if (fTransport.readStep() == false) {
-		printf("LSPReaderThread::ExecuteUnit(void) false\n");
 		Quit();
 		fTransport.Close();  //TODO: this should be performed by the Client itself not from the thread!
 		return B_ERROR;

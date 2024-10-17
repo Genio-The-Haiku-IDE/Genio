76db4dd49f7da51f900d7c908892a49d1f2338de
Stefano Ceccherini <3501519+jackburton79@users.noreply.github.com>
2024-10-17 16:06:34 +0200

    Global status bar (#442)
    Add a global status bar (empty) with a BarberPole to show build status
    Handle "building" and "finished building"
    Hide text after five seconds

---
+++ src/ui/GlobalStatusView.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013-2023, Stefano Ceccherini 
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#pragma once
+
+#include <View.h>
+
+class BarberPole;
+class BBitmap;
+class BStatusBar;
+class BStringView;
+class GlobalStatusView : public BView {
+public:
+	GlobalStatusView();
+
+	virtual void AttachedToWindow();
+	virtual void DetachedFromWindow();
+	virtual void Draw(BRect updateRect);
+	virtual void MessageReceived(BMessage *message);
+	virtual void Pulse();
+
+	virtual BSize MinSize();
+	virtual BSize MaxSize();
+
+private:
+	BarberPole*		fBarberPole;
+	BStringView*	fStringView;
+	bigtime_t		fLastStatusChange;
+	bool			fDontHideText;
+};

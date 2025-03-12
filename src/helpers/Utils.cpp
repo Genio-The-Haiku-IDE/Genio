c3ad0a46d0b1dfd00dc1ea88e50e79ee6f3c4699
Jackburton79 <stefano.ceccherini@gmail.com>
2025-03-12 22:14:47 +0100

    Also move various stuff from Utils.h to Utils.cpp.

--- src/helpers/Utils.cpp
+++ src/helpers/Utils.cpp
@@ -155,32 +155,18 @@ KeyDownMessageFilter::AllowedModifiers()
 filter_result
 KeyDownMessageFilter::Filter(BMessage* message, BHandler** target)
 {
-
-	if(message->what == B_KEY_DOWN) {
+	if (message->what == B_KEY_DOWN) {
 		const char* bytes;
 		uint32 modifiers;
 		message->FindString("bytes", &bytes);
 		modifiers = static_cast<uint32>(message->GetInt32("modifiers", 0));
 		modifiers = modifiers & AllowedModifiers();
-		if(bytes[0] == fKey && modifiers == fModifiers) {
+		if (bytes[0] == fKey && modifiers == fModifiers) {
 			Looper()->PostMessage(fCommandToSend);
 			return fFilterResult;
 		}
 	}
 	return B_DISPATCH_MESSAGE;
-}
-
-
-template<>
-entry_ref
-find_value<B_REF_TYPE>(BMessage* message, std::string name, int index)
-{
-	entry_ref ref;
-	status_t status = message->FindRef(name.c_str(), index, &ref);
-	if(status == B_OK) {
-		return ref;
-	}
-	return entry_ref();
 }
 
 
@@ -211,7 +197,102 @@ ShowNotification(const BString& group, const BString&  title, const BString&  me
 }
 
 
+template<type_code BType>
+struct property_type
+{
+	using type = void*;
+};
+
+
+template<>
+struct property_type<B_STRING_TYPE>
+{
+	using type = std::string;
+};
+
+
+template<>
+struct property_type<B_REF_TYPE>
+{
+	using type = entry_ref;
+};
+
+
+// Used by the FIND_IN_ARRAY macro
+template<type_code BType>
+typename property_type<BType>::type
+find_value(BMessage* message, std::string name, int index) {
+	typename property_type<BType>::type typed_data;
+	ssize_t size;
+	const void* data;
+	status_t status = message->FindData(name.c_str(), BType, index, &data, &size);
+	if(status == B_OK) {
+		memcpy(data, size, &typed_data);
+	}
+	return typename property_type<BType>::type();
+}
+
+
+template<>
+entry_ref
+find_value<B_REF_TYPE>(BMessage* message, std::string name, int index)
+{
+	entry_ref ref;
+	status_t status = message->FindRef(name.c_str(), index, &ref);
+	if(status == B_OK) {
+		return ref;
+	}
+	return entry_ref();
+}
+
+
+template<type_code BType>
+class message_property {
+private:
+	BMessage* message_;
+	std::string prop_name_;
+
+public:
+	class iterator {
+	private:
+		int index_;
+		BMessage* message_;
+		std::string prop_name_;
+	public:
+		iterator(BMessage* message, std::string prop_name, int index = 0)
+			: message_(message), prop_name_(prop_name), index_(index) {}
+		bool operator==(iterator &rhs) {
+			return index_ == rhs.index_ &&
+				message_ == rhs.message_ &&
+				prop_name_ == rhs.prop_name_;
+		}
+		bool operator!=(iterator &rhs) { return !(*this == rhs); }
+		iterator &operator++() { ++index_; return *this; }
+		iterator operator++(int) {
+			iterator clone(*this);
+			++index_;
+			return clone;
+		}
+		typename property_type<BType>::type operator*() {
+			return find_value<BType>(message_, prop_name_, index_);
+		}
+	};
+	message_property(BMessage* message, std::string prop_name)
+		: message_(message), prop_name_(prop_name) {}
+	iterator begin() { return iterator(message_, prop_name_, 0); }
+	iterator end() { return iterator(message_, prop_name_, size()); }
+	size_t size() {
+		int32 count;
+		if(message_->GetInfo(prop_name_.c_str(), nullptr, &count) == B_OK) {
+			return static_cast<size_t>(count);
+		}
+		return 0; // FIXME: throw an exception here
+	}
+};
+
+
 #define FIND_IN_ARRAY(ARRAY, VALUE) (std::find(std::begin(ARRAY), std::end(ARRAY), VALUE) != std::end(ARRAY));
+
 
 std::string sourceExt[] = {".cpp", ".c", ".cc", ".cxx", ".c++"}; //, ".m", ".mm"};
 std::string headerExt[] = {".h", ".hh", ".hpp", ".hxx"}; //, ".inc"};
--- src/helpers/Utils.h
+++ src/helpers/Utils.h
@@ -7,13 +7,11 @@
 #define UTILS_H
 
 
-#include <OutlineListView.h>
 #include <string>
 
 #include <Alert.h>
 #include <MessageFilter.h>
 #include <Notification.h>
-#include <ObjectList.h>
 
 
 class BBitmap;
@@ -75,93 +73,6 @@ private:
 			uint32			fCommandToSend;
 			filter_result	fFilterResult;
 };
-
-
-template<type_code BType>
-struct property_type
-{
-	using type = void*;
-};
-
-
-template<>
-struct property_type<B_STRING_TYPE>
-{
-	using type = std::string;
-};
-
-
-template<>
-struct property_type<B_REF_TYPE>
-{
-	using type = entry_ref;
-};
-
-
-// Used by the FIND_IN_ARRAY macro
-template<type_code BType>
-typename property_type<BType>::type
-find_value(BMessage* message, std::string name, int index) {
-	typename property_type<BType>::type typed_data;
-	ssize_t size;
-	const void* data;
-	status_t status = message->FindData(name.c_str(), BType, index, &data, &size);
-	if(status == B_OK) {
-		memcpy(data, size, &typed_data);
-	}
-	return typename property_type<BType>::type();
-}
-
-
-template<>
-entry_ref
-find_value<B_REF_TYPE>(BMessage* message, std::string name, int index);
-
-
-template<type_code BType>
-class message_property {
-private:
-	BMessage* message_;
-	std::string prop_name_;
-
-public:
-	class iterator {
-	private:
-		int index_;
-		BMessage* message_;
-		std::string prop_name_;
-	public:
-		iterator(BMessage* message, std::string prop_name, int index = 0)
-			: message_(message), prop_name_(prop_name), index_(index) {}
-		bool operator==(iterator &rhs) {
-			return index_ == rhs.index_ &&
-				message_ == rhs.message_ &&
-				prop_name_ == rhs.prop_name_;
-		}
-		bool operator!=(iterator &rhs) { return !(*this == rhs); }
-		iterator &operator++() { ++index_; return *this; }
-		iterator operator++(int) {
-			iterator clone(*this);
-			++index_;
-			return clone;
-		}
-		typename property_type<BType>::type operator*() {
-			return find_value<BType>(message_, prop_name_, index_);
-		}
-	};
-	message_property(BMessage* message, std::string prop_name)
-		: message_(message), prop_name_(prop_name) {}
-	iterator begin() { return iterator(message_, prop_name_, 0); }
-	iterator end() { return iterator(message_, prop_name_, size()); }
-	size_t size() {
-		int32 count;
-		if(message_->GetInfo(prop_name_.c_str(), nullptr, &count) == B_OK) {
-			return static_cast<size_t>(count);
-		}
-		return 0; // FIXME: throw an exception here
-	}
-};
-
 
 // TODO: Move to ScintillaUtils.h ?
 int32 rgb_colorToSciColor(rgb_color color);

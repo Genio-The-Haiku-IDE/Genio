789131b553c7b9fd6ec6291b882aeecda0133460
Freaxed <andrea.anzani@gmail.com>
2025-06-06 11:29:20 +0200

    HyerLink::Open amended for Genio

--- src/terminal/HyperLink.cpp
+++ src/terminal/HyperLink.cpp
@@ -6,6 +6,9 @@
 
 #include "HyperLink.h"
 
+#include <Application.h>
+#include <Entry.h>
+#include <cstdio>
 #include <errno.h>
 #include <stdlib.h>
 
@@ -37,12 +40,35 @@ HyperLink::HyperLink(const BString& text, const BString& address, Type type)
 {
 }
 
-
 status_t
 HyperLink::Open()
 {
 	if (!IsValid())
 		return B_BAD_VALUE;
+
+	if (GetType() != TYPE_URL) {
+		entry_ref  	ref;
+		if (get_ref_for_path(fAddress.String(), &ref) == B_OK &&
+			BEntry(&ref).IsDirectory() != true) {
+
+			BMessage msg(B_REFS_RECEIVED);
+			msg.AddRef("refs", &ref);
+			msg.AddBool("openWithPreferred", true);
+
+			if (GetType() == TYPE_PATH_WITH_LINE ||
+				GetType() == TYPE_PATH_WITH_LINE_AND_COLUMN) {
+				int32 pos = fAddress.FindFirst(":");
+				if (pos > 0 && pos < fAddress.Length())
+				{
+					int32 line = atoi(fAddress.Remove(0, pos+1).String());
+					if (line > 0)
+						msg.AddInt32("start:line", line);
+				}
+			}
+			be_app->PostMessage(&msg);
+			return B_OK;
+		}
+	}
 
 	// open with the "open" program
 	BString address(fAddress);

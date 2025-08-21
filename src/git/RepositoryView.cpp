49f15983908e5f4ac4ce55432e9482f0b86dbd71
Jackburton79 <stefano.ceccherini@gmail.com>
2025-08-21 08:25:07 +0200

    RepositoryView::ShowPopupMenu: refactor with fail early

--- src/git/RepositoryView.cpp
+++ src/git/RepositoryView.cpp
@@ -289,147 +289,147 @@ RepositoryView::_BuildBranchTree(const BString &branch, uint32 branchType, const
 void
 RepositoryView::ShowPopupMenu(BPoint where)
 {
-	auto optionsMenu = new BPopUpMenu("Options", false, false);
 	auto index = IndexOf(where);
-	if (index >= 0) {
-		auto item = dynamic_cast<BranchItem*>(ItemAt(index));
-		if (item == nullptr) {
-			delete optionsMenu;
-			return;
-		}
-		auto itemType = item->BranchType();
-		BString selectedBranch(item->BranchName());
-		selectedBranch.RemoveLast("*");
+	if (index < 0)
+		return;
 
-		StringFormatter fmt;
-		fmt.Substitutions["%selected_branch%"] = selectedBranch;
+	auto item = dynamic_cast<BranchItem*>(ItemAt(index));
+	if (item == nullptr)
+		return;
 
-		switch (itemType) {
-			case kLocalBranch: {
+	auto itemType = item->BranchType();
+	BString selectedBranch(item->BranchName());
+	selectedBranch.RemoveLast("*");
 
-				if (selectedBranch != fCurrentBranch) {
-					optionsMenu->AddItem(
-						new BMenuItem(
-							fmt << B_TRANSLATE("Switch to \"%selected_branch%\""),
-							new GMessage{
-								{"what", MsgSwitchBranch},
-								{"value", selectedBranch},
-								{"type", GIT_BRANCH_LOCAL},
-								{"sender", kSenderRepositoryPopupMenu}}));
-				}
+	StringFormatter fmt;
+	fmt.Substitutions["%selected_branch%"] = selectedBranch;
 
-				optionsMenu->AddItem(
-					new BMenuItem(
-						fmt << B_TRANSLATE("Rename \"%selected_branch%\""),
-						new GMessage{
-							{"what", MsgRenameBranch},
-							{"value", selectedBranch},
-							{"type", GIT_BRANCH_LOCAL}}));
+	auto optionsMenu = new BPopUpMenu("Options", false, false);
 
-				optionsMenu->AddItem(
-					new BMenuItem(
-						fmt << B_TRANSLATE("Delete \"%selected_branch%\""),
-						new GMessage{
-							{"what", MsgDeleteBranch},
-							{"value", selectedBranch},
-							{"type", GIT_BRANCH_LOCAL}}));
-
-				optionsMenu->AddSeparatorItem();
-
-				optionsMenu->AddItem(
-					new BMenuItem(
-						fmt << B_TRANSLATE("Create new branch from \"%selected_branch%\""),
-						new GMessage{
-							{"what", MsgNewBranch},
-							{"value", selectedBranch},
-							{"type", GIT_BRANCH_LOCAL}}));
-
-				// optionsMenu->AddItem(
-					// new BMenuItem(
-						// fmt << B_TRANSLATE("Create new tag from \"%selected_branch%\""),
-						// new GMessage{
-							// {"what", MsgNewTag},
-							// {"value", selected_branch}}));
-
-				break;
-			}
-			case kRemoteBranch: {
-
-				fmt.Substitutions["%current_branch%"] = fCurrentBranch;
-				LogInfo("fmt.Substitutions[%current_branch%] = %s", fCurrentBranch.String());
-
+	switch (itemType) {
+		case kLocalBranch:
+		{
+			if (selectedBranch != fCurrentBranch) {
 				optionsMenu->AddItem(
 					new BMenuItem(
 						fmt << B_TRANSLATE("Switch to \"%selected_branch%\""),
 						new GMessage{
 							{"what", MsgSwitchBranch},
 							{"value", selectedBranch},
-							{"type", GIT_BRANCH_REMOTE},
+							{"type", GIT_BRANCH_LOCAL},
 							{"sender", kSenderRepositoryPopupMenu}}));
-
-				// Deleting a remote branch is disabled for now
-				// the code in GitRepository deletes only the local ref to the remote branch and
-				// git fetch --all brings the remote branch back again
-				// TODO: A different approach is required to delete a remote branch using push
-				// optionsMenu->AddItem(
-					// new BMenuItem(
-						// fmt << B_TRANSLATE("Delete \"%selected_branch%\""),
-						// new GMessage{
-							// {"what", MsgDeleteBranch},
-							// {"value", selected_branch},
-							// {"type", GIT_BRANCH_REMOTE}}));
-
-
-				optionsMenu->AddSeparatorItem();
-
-				// We don't allow to merge a local branch into its origin
-				// if (!selected_branch.EndsWith(fCurrentBranch)) {
-					// optionsMenu->AddItem(
-						// new BMenuItem(
-							// fmt << B_TRANSLATE("Merge \"%selected_branch%\" into \"%current_branch%\""),
-							// new GMessage{
-								// {"what", MsgMerge},
-								// {"selected_branch", selected_branch},
-								// {"current_branch", fCurrentBranch}}));
-					// optionsMenu->AddSeparatorItem();
-				// }
-
-				optionsMenu->AddItem(
-					new BMenuItem(
-						fmt << B_TRANSLATE("Create new branch from \"%selected_branch%\""),
-						new GMessage{
-							{"what", MsgNewBranch},
-							{"value", selectedBranch},
-							{"type", GIT_BRANCH_REMOTE}}));
-
-				// optionsMenu->AddItem(
-					// new BMenuItem(
-						// fmt << B_TRANSLATE("Create new tag from \"%selected_branch%\""),
-						// new GMessage{
-							// {"what", MsgNewTag},
-							// {"value", selected_branch}}));
-
-				break;
 			}
-			case kTag:
-			{
-				// TODO
-				break;
-			}
-			default:
-			{
-				delete optionsMenu;
-				return;
-			}
+
+			optionsMenu->AddItem(
+				new BMenuItem(
+					fmt << B_TRANSLATE("Rename \"%selected_branch%\""),
+					new GMessage{
+						{"what", MsgRenameBranch},
+						{"value", selectedBranch},
+						{"type", GIT_BRANCH_LOCAL}}));
+
+			optionsMenu->AddItem(
+				new BMenuItem(
+					fmt << B_TRANSLATE("Delete \"%selected_branch%\""),
+					new GMessage{
+						{"what", MsgDeleteBranch},
+						{"value", selectedBranch},
+						{"type", GIT_BRANCH_LOCAL}}));
+
+			optionsMenu->AddSeparatorItem();
+
+			optionsMenu->AddItem(
+				new BMenuItem(
+					fmt << B_TRANSLATE("Create new branch from \"%selected_branch%\""),
+					new GMessage{
+						{"what", MsgNewBranch},
+						{"value", selectedBranch},
+						{"type", GIT_BRANCH_LOCAL}}));
+
+			// optionsMenu->AddItem(
+				// new BMenuItem(
+					// fmt << B_TRANSLATE("Create new tag from \"%selected_branch%\""),
+					// new GMessage{
+						// {"what", MsgNewTag},
+						// {"value", selected_branch}}));
+
+			break;
 		}
+		case kRemoteBranch:
+		{
+			fmt.Substitutions["%current_branch%"] = fCurrentBranch;
+			LogInfo("fmt.Substitutions[%current_branch%] = %s", fCurrentBranch.String());
 
-		optionsMenu->AddItem(
-			new BMenuItem(
-				fmt << B_TRANSLATE("Copy name"),
-				new GMessage{
-					{"what", MsgCopyRefName},
-					{"value", selectedBranch}}));
+			optionsMenu->AddItem(
+				new BMenuItem(
+					fmt << B_TRANSLATE("Switch to \"%selected_branch%\""),
+					new GMessage{
+						{"what", MsgSwitchBranch},
+						{"value", selectedBranch},
+						{"type", GIT_BRANCH_REMOTE},
+						{"sender", kSenderRepositoryPopupMenu}}));
+
+			// Deleting a remote branch is disabled for now
+			// the code in GitRepository deletes only the local ref to the remote branch and
+			// git fetch --all brings the remote branch back again
+			// TODO: A different approach is required to delete a remote branch using push
+			// optionsMenu->AddItem(
+				// new BMenuItem(
+					// fmt << B_TRANSLATE("Delete \"%selected_branch%\""),
+					// new GMessage{
+						// {"what", MsgDeleteBranch},
+						// {"value", selected_branch},
+						// {"type", GIT_BRANCH_REMOTE}}));
+
+
+			optionsMenu->AddSeparatorItem();
+
+			// We don't allow to merge a local branch into its origin
+			// if (!selected_branch.EndsWith(fCurrentBranch)) {
+				// optionsMenu->AddItem(
+					// new BMenuItem(
+						// fmt << B_TRANSLATE("Merge \"%selected_branch%\" into \"%current_branch%\""),
+						// new GMessage{
+							// {"what", MsgMerge},
+							// {"selected_branch", selected_branch},
+							// {"current_branch", fCurrentBranch}}));
+				// optionsMenu->AddSeparatorItem();
+			// }
+
+			optionsMenu->AddItem(
+				new BMenuItem(
+					fmt << B_TRANSLATE("Create new branch from \"%selected_branch%\""),
+					new GMessage{
+						{"what", MsgNewBranch},
+						{"value", selectedBranch},
+						{"type", GIT_BRANCH_REMOTE}}));
+
+			// optionsMenu->AddItem(
+				// new BMenuItem(
+					// fmt << B_TRANSLATE("Create new tag from \"%selected_branch%\""),
+					// new GMessage{
+						// {"what", MsgNewTag},
+						// {"value", selected_branch}}));
+
+			break;
+		}
+		case kTag:
+		{
+			// TODO
+			break;
+		}
+		default:
+		{
+			return;
+		}
 	}
+
+	optionsMenu->AddItem(
+		new BMenuItem(
+			fmt << B_TRANSLATE("Copy name"),
+			new GMessage{
+				{"what", MsgCopyRefName},
+				{"value", selectedBranch}}));
 
 	optionsMenu->SetTargetForItems(Target());
 	optionsMenu->Go(ConvertToScreen(where), true);

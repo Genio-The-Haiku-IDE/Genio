c4c9857ebe098813285c8ab23e3a4e41039700ce
Jackburton79 <stefano.ceccherini@gmail.com>
2025-08-30 15:20:45 +0200

    Small style changes

--- src/ui/TerminalTab.cpp
+++ src/ui/TerminalTab.cpp
@@ -8,7 +8,6 @@
 
 #include <Messenger.h>
 
-#include <cassert>
 #include <cstdio>
 #include <sys/wait.h>
 
@@ -43,8 +42,8 @@ TerminalTab::AttachedToWindow()
 	if (fTermView == nullptr) {
 		fTermView = TerminalManager::CreateNewTerminal(BRect(0, 0, 100,100), BMessenger(this), fCommand);
 		fTermView->SetResizingMode(B_FOLLOW_NONE);
-		fTermView->SetExplicitMinSize(BSize(100,100));
-		fTermView->SetExplicitPreferredSize(BSize(100,100));
+		fTermView->SetExplicitMinSize(BSize(100, 100));
+		fTermView->SetExplicitPreferredSize(BSize(100, 100));
 		fTermView->SetExplicitMaxSize(BSize(B_SIZE_UNLIMITED, B_SIZE_UNLIMITED));
 		AddChild(fTermView);
 	}
